8d216d6f8b98f95d1ccef14021009e1e61cfbeb8
rijal.sarthak@gmail.com <rijal.sarthak@gmail.com>
2019-03-13 22:04:50 -0700

    Compression Working

--- HuffmanCode.java
+++ HuffmanCode.java
@@ -4,15 +4,16 @@ import java.util.*;
 public class HuffmanCode{
 
 	private Queue<HuffmanNode> pQueue;
+	private HuffmanNode root;
 
 
-	private class HuffmanNode implements Comparator<HuffmanNode>{
-   	public String character;
+	private class HuffmanNode implements Comparable<HuffmanNode> {
+   	public int character;
    	public int frequency;
    	public HuffmanNode left;
    	public HuffmanNode right;
    
-   	public HuffmanNode(String character, int frequency, 
+   	public HuffmanNode(int character, int frequency, 
    					HuffmanNode left, HuffmanNode right ){
    		this.character = character;
    		this.frequency = frequency;
@@ -21,11 +22,12 @@ public class HuffmanCode{
 	}
 
 
-	public HuffmanNode(String character, int frequency){
-		this.character = character;
-		this.frequency = frequency;
-		this.left = null;
-		this.right = null;
+	public HuffmanNode(int character, int frequency){
+		this(character, frequency, null, null);
+	}
+
+	public HuffmanNode(int character){
+		this(character, -1, null, null);
 	}
 		   
    
@@ -48,8 +50,7 @@ public class HuffmanCode{
 		pQueue = new PriorityQueue<HuffmanNode>();
 		for (int i = 0; i < frequencies.length; i++){
 			if (frequencies[i] != 0){
-				char character = frequencies.toString((char) i);
-				pQueue.add(new HuffmanNode(character, frequencies[i]));
+				pQueue.add(new HuffmanNode(i, frequencies[i]));
 				
 			}
 		}
@@ -57,44 +58,85 @@ public class HuffmanCode{
 		while (pQueue.size() != 1){
 			HuffmanNode one = pQueue.remove();
 			HuffmanNode two = pQueue.remove();
-			HuffmanNode combine = new HuffmanNode(null, one.frequency+two.frequency, one, two);
+			HuffmanNode combine = new HuffmanNode(-1, one.frequency+two.frequency, one, two);
 			pQueue.add(combine);
-      }
+		  }
+		root = pQueue.remove();
     }
 
-	private void makeCode(){
-
-	}
     // This constructor should initialize a new HuffmanCode object by reading in a previously constructed
     // code from a .code file. You may assume the Scanner is not null and is always contains data
     // encoded in legal, valid standard format.
     public HuffmanCode(Scanner input){
+		root = new HuffmanNode(-1);
+		while (input.hasNextLine()){
+			int character = Integer.parseInt(input.nextLine());
+			String path = input.nextLine();
+			root = buildTree(root, character, path);
+		}
 
-    }
+	}
+	
+	private HuffmanNode buildTree(HuffmanNode node, int character, String path){
+		if (node == null){
+			node = new HuffmanNode(-1);
+		}
+		if (path.length() == 1){
+			if (path.substring(0).equals("0")){
+				node.left = new HuffmanNode(character);
+			} else {
+				node.right = new HuffmanNode(character);
+			}
+	
+		}
+		else if (path.substring(0,1).equals("0")){
+			node.left = buildTree(node.left, character, path.substring(1));
+		} else {
+			node.right = buildTree(node.right, character, path.substring(1));
+		}
+
+		return node;
+
+
+	}
 
 //  This method should store the current huffman codes to the given output stream in the standard
 //  format described above.
     public void save(PrintStream output){
-
-    }
+		writeCode(root, output, "");
+	}
+	
+	private void writeCode(HuffmanNode node, PrintStream output, String str){
+		if (node != null){
+			if (node.left == null && node.right == null){
+				output.println(node.character);
+				output.println(str);
+			} else {
+				writeCode(node.left, output, str+"0");
+				writeCode(node.right, output, str+"1");
+			}
+		}
+	}
 // This method should read individual bits from the input stream and write the corresponding characters
 // to the output. It should stop reading when the BitInputStream is empty. You may assume that
 // the input stream contains a legal encoding of characters for this tree’s huffman code. See below
 // for the methods that a BitInputStream has.
     public void translate(BitInputStream input, PrintStream output){
+		while (input.hasNextBit()){
+			HuffmanNode temp = root;
+			while (temp.left != null && temp.right != null){
+				if (input.nextBit() == 0){
+					temp = temp.left;
+				} else {
+					temp = temp.right;
+				}
+			}
+			output.print((char)temp.character);
 
-    }
+		}
+	}
+	
 
-//     This method returns the next bit in the input stream. If there is no such bit, then it throws a
-// NoSuchElementException.
-    public int nextBit(){
-        return 0;
-    }
-    
-    //This method returns true if the input stream has at least one more bit and false otherwise.
-    public boolean hasNextBit(){
-        return true;
-    }
 
 }
 
